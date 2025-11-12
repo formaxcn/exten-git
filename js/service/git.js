6407651b166bcc853875a08a5d3cb9bd5b5125d3
formaxcn <tjrtzh@gmail.com>
2025-11-12 11:33:06 +0800

    module

--- js/service/git.js
+++ js/service/git.js
@@ -3,9 +3,9 @@
 // ES6模块导入isomorphic-git相关库
 // 注意：在Service Worker环境中需要正确配置这些依赖
 
-import git from 'isomorphic-git';
-import { LightningFS } from '@isomorphic-git/lightning-fs';
-import GitHttp from 'isomorphic-git/http/web';
+import git from '../lib/isomorphic-git.index.js';
+import LightningFS from '../lib/lightning-fs.min.js';
+import GitHttp from '../lib/isomorphic-git-http-web.index.js';
 
 class GitManager {
   constructor() {
